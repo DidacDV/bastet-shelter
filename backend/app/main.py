92f0b7766dbb51737e3aed6053a619322eaf9759
DidacDV <keigratzdac@gmail.com>
2026-03-07 18:04:29 +0100

    Backend - enable local web requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Bastet-Shelter")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def root():
     return {"message": "bastet is running"}

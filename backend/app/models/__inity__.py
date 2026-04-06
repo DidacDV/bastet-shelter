662e3f157848d0a3bb54d755d26a3ff971f3df78
DidacDV <keigratzdac@gmail.com>
2026-04-06 12:53:23 +0200

    Backend/Task#89 - Init.py for model importing

---
+++ backend/app/models/__inity__.py
@@ -0,0 +1,12 @@
+from app.models.animal import Animal, animal_trait_association
+from app.models.trait import Trait
+from app.models.shelter import Shelter
+from app.models.refuge import Refuge
+from app.models.province import Province
+from app.models.shelter_member import ShelterMember
+from app.models.user import User
+from app.models.login import Login
+from app.models.task.task import Task
+from app.models.task.shift_task import ShiftTask
+from app.models.shift.shift import Shift
+from app.models.shift.shift_participant import ShiftParticipant

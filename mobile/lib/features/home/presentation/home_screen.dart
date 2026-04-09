067fd91484c9ef3a9a1dd3733f4866266acd1f33
DidacDV <keigratzdac@gmail.com>
2026-04-09 15:53:47 +0200

    Mobile/Global - Move tabs to its correct folder

--- mobile/lib/features/home/presentation/home_screen.dart
+++ mobile/lib/features/home/presentation/home_screen.dart
@@ -1,12 +1,12 @@
 import 'package:bastetshelter/core/constants.dart'; // Ensure AppColors is imported
+import 'package:bastetshelter/features/community/presentation/community_screen.dart';
+import 'package:bastetshelter/features/shifts/presentation/shifts_screen.dart';
+import 'package:bastetshelter/features/tasks/presentation/tasks_screen.dart';
 import 'package:bastetshelter/providers/geo/geo_provider.dart';
 import 'package:bastetshelter/features/animals/presentation/animals_screen.dart';
 import 'package:bastetshelter/features/common/components/app_shell.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'shifts_screen.dart';
-import 'tasks_screen.dart';
-import 'community_screen.dart';
 import 'package:bastetshelter/features/home/presentation/home_tab.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {

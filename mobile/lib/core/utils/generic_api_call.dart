b0b92130bbe75904cc2986f6fddd2e9b3c559a2a
DidacDV <keigratzdac@gmail.com>
2026-04-13 17:28:54 +0200

    Mobile/Task#120 - Pass type to generic api call

--- mobile/lib/core/utils/generic_api_call.dart
+++ mobile/lib/core/utils/generic_api_call.dart
@@ -1,15 +1,17 @@
 import 'package:bastetshelter/core/navigation_service.dart';
 import 'package:bastetshelter/core/network/api_client.dart';
 
-Future<void> genericApiCall(Future<void> Function() externalCall) async {
+Future<T?> genericApiCall<T>(Future<T> Function() externalCall) async {
   try {
-    await externalCall();
+    return await externalCall();
   } on ApiException catch (e) {
     NavigationService.instance.showSnackBar(e.message, isError: true);
+    return null;
   } catch (_) {
     NavigationService.instance.showSnackBar(
       'Something went wrong',
       isError: true,
     );
+    return null;
   }
 }

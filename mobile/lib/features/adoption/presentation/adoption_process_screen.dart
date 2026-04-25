444cc56a6abe840fe7076c14d3ae4bc3b0fe1139
DidacDV <keigratzdac@gmail.com>
2026-04-25 16:27:40 +0200

    Mobile/Task#139 - Add repositories to service locator & provider wrapper

--- mobile/lib/features/adoption/presentation/adoption_process_screen.dart
+++ mobile/lib/features/adoption/presentation/adoption_process_screen.dart
@@ -1,27 +1,37 @@
-import 'package:bastetshelter/core/constants.dart';
 import 'package:bastetshelter/features/common/components/layout/app_bar.dart';
+import 'package:bastetshelter/providers/adoption/adoption_screen_data_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AdoptionProcessScreen extends StatelessWidget {
+class AdoptionProcessScreen extends ConsumerWidget {
   final int adoptionProcessId;
 
   const AdoptionProcessScreen({super.key, required this.adoptionProcessId});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Watch the composite provider!
+    final screenDataAsync = ref.watch(
+      adoptionScreenDataProvider(adoptionProcessId),
+    );
+
     return Scaffold(
-      backgroundColor: AppColors.background,
       appBar: const BastetAppBar(
         customTitle: 'Adoption Process',
         showBackButton: true,
-        showConfig: false,
-        showLogout: false,
       ),
-      body: Center(
-        child: Text(
-          'Process ID: $adoptionProcessId',
-          style: Theme.of(context).textTheme.bodyLarge,
-        ),
+      body: screenDataAsync.when(
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (e, _) => Center(child: Text('Error loading data')),
+        data: (data) {
+          return Column(
+            children: [
+              Text('Process Status: ${data.process.status.name}'),
+              Text('Animal Name: ${data.animal.name}'),
+              Text('Adoptant Email: ${data.adoptant.email}'),
+            ],
+          );
+        },
       ),
     );
   }

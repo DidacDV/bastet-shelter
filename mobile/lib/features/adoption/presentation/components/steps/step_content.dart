fff92e71d9af320b325d88e9382b9cceff5a9c64
DidacDV <keigratzdac@gmail.com>
2026-04-26 18:40:00 +0200

    Mobile/Task#145 - Implement common view for steps that only have schedule as extra option

--- mobile/lib/features/adoption/presentation/components/steps/step_content.dart
+++ mobile/lib/features/adoption/presentation/components/steps/step_content.dart
@@ -1,7 +1,13 @@
 import 'package:bastetshelter/features/adoption/data/adoption_enums.dart';
 import 'package:bastetshelter/features/adoption/data/models/adoption_steps/adoption_step_details.dart';
+import 'package:bastetshelter/features/adoption/data/models/adoption_steps/steps/animal_pickup_step_details.dart';
 import 'package:bastetshelter/features/adoption/data/models/adoption_steps/steps/form_step_details.dart';
+import 'package:bastetshelter/features/adoption/data/models/adoption_steps/steps/interview_step_details.dart';
+import 'package:bastetshelter/features/adoption/data/models/adoption_steps/steps/shelter_visit_step_details.dart';
 import 'package:bastetshelter/features/adoption/presentation/components/steps/step_form_view.dart';
+import 'package:bastetshelter/features/adoption/presentation/components/steps/step_interview_view.dart';
+import 'package:bastetshelter/features/adoption/presentation/components/steps/step_pickup_view.dart';
+import 'package:bastetshelter/features/adoption/presentation/components/steps/step_shelter_visit_view.dart';
 import 'package:flutter/material.dart';
 
 class StepContent extends StatelessWidget {
@@ -16,14 +22,20 @@ class StepContent extends StatelessWidget {
         step: step as FormStepDetails,
         processId: processId,
       ),
-      // TODO: Handle this case.
-      StepType.interview => throw UnimplementedError(),
-      // TODO: Handle this case.
-      StepType.shelterVisit => throw UnimplementedError(),
-      // TODO: Handle this case.
+      StepType.interview => InterviewStepView(
+        step: step as InterviewStepDetails,
+        processId: processId,
+      ),
+      StepType.shelterVisit => ShelterVisitStepView(
+        step: step as ShelterVisitStepDetails,
+        processId: processId,
+      ),
       StepType.contract => throw UnimplementedError(),
-      // TODO: Handle this case.
-      StepType.animalPickup => throw UnimplementedError(),
+
+      StepType.animalPickup => AnimalPickupStepView(
+        step: step as AnimalPickupStepDetails,
+        processId: processId,
+      ),
     };
   }
 }

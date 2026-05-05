1a11517e4569d25ee4d3177f41ba6308809e8db3
DidacDV <keigratzdac@gmail.com>
2026-05-05 20:54:31 +0200

    Backend/Task#163 - Impl missing CRUD endpoints for tasks

--- backend/app/schemas/task_schema/task_schema.py
+++ backend/app/schemas/task_schema/task_schema.py
@@ -15,3 +15,11 @@ class TaskResponse(BaseModel):
     description: str
     num_people: int
     shelter_id: int
+
+class TaskResponseList(BaseModel):
+    tasks: list[TaskResponse]
+
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    num_people: int | None = None
